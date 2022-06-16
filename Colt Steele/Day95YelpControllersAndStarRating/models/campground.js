09f1cb25883416c9c2da0efaf77363aabdb72491
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-06-16 20:20:56 +0530

    Day 95 Controller
    Controllers for all the routes and adding star for reviews.

---
+++ Colt Steele/Day95YelpControllersAndStarRating/models/campground.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("../models/review");
+
+// Campground Schema
+const CampgroundSchema = new Schema({
+  title: String,
+  price: Number,
+  description: String,
+  location: String,
+  imageUrl: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+// Add middleware for deleting reviews inside camp
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
+});
+
+module.exports = mongoose.model("Campground", CampgroundSchema);
