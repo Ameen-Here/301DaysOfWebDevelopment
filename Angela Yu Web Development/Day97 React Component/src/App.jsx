1be30041dde3aaf49f1fbaebd03aa9dbd0e77109
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-06-17 16:43:56 +0530

    Day 97
    Css inside jsx, inline css in jsx and adding class in jsx. React components

---
+++ Angela Yu Web Development/Day97 React Component/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Import Header component
+import Heading from "./Heading.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Heading />
+    </div>
+  );
+};
+
+export default App;
---
+++ Angela Yu Web Development/Day97 React Component/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Import App component
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
