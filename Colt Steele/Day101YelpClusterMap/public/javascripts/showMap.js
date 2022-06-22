7a97c2e63a216ecbc8faaec4efe0e656f474ff3d
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-06-22 16:44:24 +0530

    Day 101 Cluster Map
    Added new cluster map to index which show all the available campsite on the map as cluster. User can move down to the campsite by clicking the popup in map.

---
+++ Colt Steele/Day101YelpClusterMap/public/javascripts/showMap.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  center: locationMap, // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+// Create a new marker.
+const marker = new mapboxgl.Marker().setLngLat(locationMap).addTo(map);
