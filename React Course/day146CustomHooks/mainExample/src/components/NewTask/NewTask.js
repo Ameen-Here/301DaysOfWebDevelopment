fd1ed9a617df73eb6ac690dce618445852a4c2d0
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-08-10 20:34:07 +0530

    Day 146 Custom hooks
    Made custom hooks for http request and handling datas, used firebase server to handle backends.

---
+++ React Course/day146CustomHooks/mainExample/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendRequestData } = useHttp();
+
+  const dataHandler = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+  const enterTaskHandler = async (taskText) => {
+    sendRequestData(
+      {
+        url: "YOUR FIREBASE URL",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: { text: taskText },
+      },
+      dataHandler.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
